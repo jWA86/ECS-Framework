98444828956e4f03487f3c1c7ed65ead80f3c710
jojoW10 <defrance06@gmail.com>
2017-07-02 14:02:10 +0200

    initial test

---
+++ test/index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var chai_1 = require("chai");
+describe("initial test", function () {
+    it("false should be equal to false", function () {
+        chai_1.expect(true).to.equal(false);
+    });
+});
---
+++ test/index.ts
@@ -0,0 +1,8 @@
+import {expect} from "chai";
+
+describe(`initial test`, () => {
+	it(`false should be equal to false`, ()=> {
+		expect(false).to.equal(false);
+	});
+});
+
