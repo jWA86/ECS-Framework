9a298a436712727edd60473c6649f0358be44474
jojoW10 <defrance06@gmail.com>
2017-07-02 14:29:41 +0200

    added types for mocha / chai

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
 "use strict";
-exports.__esModule = true;
-var chai_1 = require("chai");
-describe("initial test", function () {
-    it("false should be equal to false", function () {
-        chai_1.expect(true).to.equal(false);
+Object.defineProperty(exports, "__esModule", { value: true });
+require("mocha");
+const chai_1 = require("chai");
+describe(`initial test`, () => {
+    it(`false should be equal to true`, () => {
+        chai_1.expect(true).to.equal(true);
     });
 });
--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,9 @@
+import "mocha";
 import {expect} from "chai";
 
 describe(`initial test`, () => {
-	it(`false should be equal to false`, ()=> {
-		expect(false).to.equal(false);
-	});
+	it(`false should be equal to true`, () => {
+		expect(true).to.equal(true);
+	}); 
 });
 
