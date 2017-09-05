87a3c70152b1e8a6675d08c314084a5ff47e6405
jojoW10 <defrance06@gmail.com>
2017-09-05 19:09:10 +0200

    add karma support

---
+++ karma.conf.js
@@ -0,0 +1,19 @@
+module.exports = function(config) {
+  config.set({
+      frameworks: ["mocha", "karma-typescript"],
+      files: [
+          { pattern: "src/**/*.ts" },
+          "node_modules/es6-shim/es6-shim.js"
+      ],
+      preprocessors: {
+          "**/*.ts": ["karma-typescript"]
+          
+      },
+      reporters: ["dots", "karma-typescript"],
+
+      browsers: ["Chrome", "Firefox", "IE", "PhantomJS"],
+
+      singleRun: false,
+      concurrency: Infinity
+  });
+};
