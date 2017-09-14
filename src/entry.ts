27a190e35b68fe2b61ebdfccb06f1ee5d10bb36b
jojoW10 <defrance06@gmail.com>
2017-09-14 19:06:15 +0200

    bundle for browser

--- src/entry.ts
+++ src/entry.ts
@@ -1,3 +1,4 @@
 import { ComponentFactory, EntityFactory } from './ComponentFactory';
 import { System, MultiParallelSystem, MultiNonParallelSystem } from './System';
-import { ISystem, IComponent, IComponentFactory, IEntityFactory } from "./interfaces";
+
+export {ComponentFactory, EntityFactory, System, MultiNonParallelSystem, MultiParallelSystem}
