6dff261aa4496d1b011016b0b0eca011d3c90d1e
JOJO_Mac <Defrance06@gmail.com>
2017-12-07 15:55:21 +0100

    lint interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,8 +1,20 @@
-export { ISystem, IComponent, IPool, IComponentFactory, IEntityFactory }
+export { ISystem, IComponent, IPool, IComponentFactory, IEntityFactory };
+
+interface IFrameEvent {
+    /* The number of times the frame event was fired */
+    count: number;
+    /* The time passed in seconds since the last frame event */
+    delta: number;
+    loopCount: number;
+    reverse: boolean;
+    /* The total amount of time passed since the first frame event in seconds */
+    time: number;
+}
 
 interface ISystem {
-    process(factory:IComponentFactory<IComponent>, ...args: any[]);
-    execute(...args: any[]);
+    setFactories(... args: Array<IComponentFactory<IComponent>>);
+    process();
+    execute(... args: any[]);
 }
 
 interface IComponent {
@@ -11,32 +23,53 @@ interface IComponent {
 }
 
 interface IPool {
-    activateAll(value: boolean); // set the active proprety of all component in the pool
-    create(entityId: number, active: boolean); // create a component with the provided values
-    delete(entityId: number): boolean; // delete a component by its id if it's in the pool     
-    get(entityId: number); // get a component by its id 
-    has(entityId: number): boolean; // does the pool contain a component with this id 
-    iterationLength: number // return the length to iterate created components and avoid iterate maximum number of zeored components
-    nbActive: number; // nb of created active components
-    nbCreated: number; // nb actives and inactives created components
-    nbFreeSlot: number; // nb of zeroed components ( free slot for creating components)
-    nbInactive: number; // nb of created inactive components
-    resize(size: number); // resize the pool, either add zeroed components or delete last components (created or zeroed indistinctly)    
-    size: number; // return the size of the pool
+    /* return the length to iterate created components and avoid iterate maximum number of zeored components
+    */
+    iterationLength: number;
+    /* nb of created active components */
+    nbActive: number;
+    /* Nb actives and inactives created components */
+    nbCreated: number;
+    /* Nb of zeroed components ( free slot for creating components) */
+    nbFreeSlot: number;
+    /* Nb of created inactive components */
+    nbInactive: number;
+    /* Return the size of the pool */
+    size: number;
+    /* Set the active proprety of all component in the pool */
+    activateAll(value: boolean);
+    /* Create a component with the provided values*/
+    create(entityId: number, active: boolean);
+    /* Delete a component by its id if it's in the pool */
+    delete(entityId: number): boolean;
+    /* Get a component by its id */
+    get(entityId: number);
+    /* Does the pool contain a component with this id */
+    has(entityId: number): boolean;
+    /* Resize the pool, either add zeroed components or delete last components (created or zeroed indistinctly) */
+    resize(size: number);
 }
 
 interface IComponentFactory<T extends IComponent> extends IPool {
-    activate(entityId: number, value: boolean); // set the ative proprety of a component
-    clear(); // empty the pool from zeroed and created components 
-    keys: Map<number, number>; // return keys of all created components and their index in the values array
-    length: number; // same as size
-    push(key: number, value: T); // add components but instanciated outside the pool (should probably not be used)
-    set(key: number, value: T); // same as push
-    values: T[]; // hold components, use for iteration in Systems. 
+    /* Return keys of all created components and their index in the values array */
+    keys: Map<number, number>;
+    /* Same as size */
+    length: number;
+    /* Hold components, use for iteration in Systems. */
+    values: T[];
+    /* Add components but instanciated outside the pool (should probably not be used) */
+    push(key: number, value: T);
+    /* Same as push */
+    set(key: number, value: T);
+    /* Set the ative proprety of a component */
+    activate(entityId: number, value: boolean);
+    /* Empty the pool from zeroed and created components */
+    clear();
 }
 
 interface IEntityFactory extends IPool {
     addFactory(name: string, factory: IComponentFactory<IComponent>);
-    getComponent(entityId: number, factoryName: string): IComponent; // get components by providing the entityId and the factory    
+    getComponent(entityId: number, factoryName: string): IComponent;
+    /* get components by providing the entityId and the factory */
     getFactory(name: string): IComponentFactory<IComponent>;
 }
