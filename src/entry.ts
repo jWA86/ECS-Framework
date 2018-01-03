cdcc5c43819e297bd6e1a117670a8245032a57a9
JOJO_Mac <Defrance06@gmail.com>
2018-01-03 20:05:23 +0100

    forgot to export GameLoop

--- src/entry.ts
+++ src/entry.ts
@@ -1,7 +1,7 @@
 import { ComponentFactory, EntityFactory, IComponent, IComponentFactory, IEntityFactory, IPool } from "./ComponentFactory";
-import { IFrameEvent } from "./GameLoop";
+import { GameLoop, IFrameEvent } from "./GameLoop";
 import "./interfaces";
 import { ISystem, System } from "./System";
 import { ISystemWithStates, SystemManager } from "./SystemManager";
 import { TimeMeasure } from "./TimeMeasure";
-export { ComponentFactory, EntityFactory, IComponent, IComponentFactory, IEntityFactory, IFrameEvent, IPool, System, ISystem, ISystemWithStates, SystemManager };
+export { ComponentFactory, EntityFactory, IComponent, IComponentFactory, IEntityFactory, IFrameEvent, IPool, GameLoop, System, ISystem, ISystemWithStates, SystemManager };
