967f0612624bf76f307d5635ba44b15f692da483
JOJO_Mac <Defrance06@gmail.com>
2018-09-16 14:19:04 +0200

    export ParameterSource Iterator&Binding

--- src/entry.ts
+++ src/entry.ts
@@ -3,6 +3,7 @@ import { EntityFactory } from "./EntityFactory";
 import { GameLoop } from "./GameLoop";
 import { Graphics } from "./Graphics";
 import * as interfaces from "./interfaces";
+import { IParameterBinding, ParameterBinding, ParametersSourceIterator  } from "./ParameterSource";
 import { RANDOM, TIMESTAMP } from "./pollyFill";
 import { Project } from "./Project";
 import { IKeyMapping, System } from "./System";
@@ -11,4 +12,4 @@ import * as DEFAULT_CONF from "./utils/DefaultConfig";
 import { HtmlInterface } from "./utils/HtmlInterface";
 import { TimeMeasureComponent, TimeMeasureSystem, TimeMeasureSystemEndMark, TimeMeasureSystemStartMark,  TimeMeasureUtil } from "./utils/TimeMeasureUtil";
 
-export { ComponentFactory, EntityFactory, interfaces, IKeyMapping, GameLoop, Graphics, HtmlInterface, Project, System, SystemManager, TimeMeasureComponent, TimeMeasureSystem, TimeMeasureSystemEndMark, TimeMeasureSystemStartMark,  TimeMeasureUtil, TIMESTAMP, RANDOM, DEFAULT_CONF };
+export { ComponentFactory, EntityFactory, interfaces, IKeyMapping, GameLoop, Graphics, HtmlInterface, Project, IParameterBinding, ParameterBinding, ParametersSourceIterator, System, SystemManager, TimeMeasureComponent, TimeMeasureSystem, TimeMeasureSystemEndMark, TimeMeasureSystemStartMark,  TimeMeasureUtil, TIMESTAMP, RANDOM, DEFAULT_CONF };
